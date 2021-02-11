01911207b028103e95a32a840d4ca7ad4249e0fc
Luis <luisca85@gmail.com>
2021-02-11 15:24:37 -0300

    arreglo 2

--- wp-content/themes/understrap/single.php
+++ wp-content/themes/understrap/single.php
@@ -30,12 +30,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<?php understrap_post_nav(); ?>
 
-				<!-- 	<?php
+					<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
 						comments_template();
 					endif;
-					?> -->
+					?>
 
 				<?php endwhile; // end of the loop. ?>
 
